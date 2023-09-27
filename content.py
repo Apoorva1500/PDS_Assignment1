739a54f5b11d8e723fedd20d725d30e1474816d1
Apoorva1500 <73296695+Apoorva1500@users.noreply.github.com>
2023-09-27 02:37:06 -0500

    Update content.py

--- content.py
+++ content.py
@@ -1,1 +1,17 @@
+# Import necessary libraries for analysis and visualization
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Load the cleaned dataset
+data = pd.read_csv("datacleaned.csv")
+
+# Calculate correlations and create visualizations
+correlation_matrix = data.corr()
+
+# Example: Create a heatmap to visualize correlations
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
+plt.title("Correlation Heatmap")
+plt.savefig("ns2/CorrelationHeatmap.png")
+plt.show()
